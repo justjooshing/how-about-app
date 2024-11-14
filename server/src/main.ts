740ed38f463a348099f0ba2e01fcb9ab48434ee6
Josh <joshua.l.mertens@gmail.com>
2024-11-14 17:50:35 +0800

    chore: add cf pages urls to cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './routes/root/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:8081'] });
+  app.enableCors({
+    origin: [
+      'http://localhost:8081',
+      'https://how-about-app-user.pages.dev/',
+      'https://how-about-app-venue.pages.dev/',
+    ],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
