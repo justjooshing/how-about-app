280ce1dfb453ba3b9eb682a3134c9cf3bcc52f5a
Josh <joshua.l.mertens@gmail.com>
2024-11-14 17:39:15 +0800

    feat: enable cors for localhost

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './routes/root/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: ['http://localhost:8081'] });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
