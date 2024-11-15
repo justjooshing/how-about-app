c57ddfce339dc83a5109ecc7628f83b7a297a115
Josh <joshua.l.mertens@gmail.com>
2024-11-15 08:46:45 +0800

    fix: remove trailing slash from cors whitelits

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:8081',
-      'https://how-about-app-user.pages.dev/',
-      'https://how-about-app-venue.pages.dev/',
+      'https://how-about-app-user.pages.dev',
+      'https://how-about-app-venue.pages.dev',
     ],
   });
   await app.listen(process.env.PORT ?? 3000);
