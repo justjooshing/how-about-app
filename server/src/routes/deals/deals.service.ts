27242d5d76e8d28cbc6d3092be74ea6f892a94d0
Josh <joshua.l.mertens@gmail.com>
2024-11-18 11:33:33 +0800

    feat(server): update api with deal_options

--- server/src/routes/deals/deals.service.ts
+++ server/src/routes/deals/deals.service.ts
@@ -7,13 +7,24 @@ const cleanDeals = (fields: TypeDealFields): ApiDeal => {
   return {
     ...fields,
     tags: fields.tags || [],
-    total_available: fields.total_available || null,
+    // total_available: fields.total_available || null,
     // @ts-expect-error cannot map banner_images
     banner_images: fields.banner_images.map((image) => image.fields.file.url),
     owner: {
       name: fields.owner[0].fields.name,
       logo: fields.owner[0].fields.logo.fields.file.url,
     },
+    // @ts-expect-error cannot map owned_deal_options
+    owned_deal_options: fields.owned_deal_options.map(
+      ({
+        fields: { original_price, discounted_price, name, total_available },
+      }) => ({
+        name,
+        original_price,
+        discounted_price,
+        total_available,
+      }),
+    ),
   };
 };
 
@@ -32,7 +43,7 @@ export class DealsService {
         field_name: 'Burgers and Pints for Two People',
       });
       const cleanedDeals = response.items.map((item) =>
-        // @ts-expect-error things title is string
+        // @ts-expect-error thinks title is Symbol<string>
         cleanDeals(item.fields),
       );
       return { deals: cleanedDeals };
--- server/types/api/deals.ts
+++ server/types/api/deals.ts
@@ -1,12 +1,21 @@
 import { TypeDealFields } from '../generated';
 
 export interface ApiDeal
-  extends Omit<TypeDealFields, 'owner' | 'tags' | 'banner_images'> {
+  extends Omit<
+    TypeDealFields,
+    'owned_deal_options' | 'owner' | 'tags' | 'banner_images'
+  > {
   tags: TypeDealFields['tags'] | [];
   banner_images: string[];
   owner: {
     name: string;
     logo: string;
+  };
+  owned_deal_options: {
+    original_price: number;
+    discounted_price: number;
+    name: string;
+    total_available: number;
   };
 }
 
