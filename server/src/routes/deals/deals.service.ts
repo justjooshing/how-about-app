aaa16ecb2e178702b105528f313c4d37e70e4b92
Josh <joshua.l.mertens@gmail.com>
2024-11-19 13:58:26 +0800

    fix: update server target file, add better contentful deal ts-expect-error messages

--- server/src/routes/deals/deals.service.ts
+++ server/src/routes/deals/deals.service.ts
@@ -10,16 +10,17 @@ import { ContentfulDeliveryService } from '@/server/src/third-party/contentful-d
 const cleanDeals = (fields: TypeDealSkeleton['fields']): ApiDeal['fields'] => {
   return {
     ...fields,
-    title: fields.title.values.toString(),
-    // @ts-expect-error error
-    tags: fields.tags['item']['values'],
-    // @ts-expect-error error
+    currency: fields.currency.values,
+    title: fields.title.toString(),
+    // @ts-expect-error Array<Symbol<Tags> isn't Tags[]
+    tags: fields.tags,
+    // @ts-expect-error cannot iterate on banner_images
     banner_images: fields.banner_images.map((image) => image.fields.file.url),
     owner: {
       name: fields.owner[0].fields.name,
       logo: fields.owner[0].fields.logo.fields.file.url,
     },
-    // @ts-expect-error error
+    // @ts-expect-error cannot iterate on owned_deal_options
     owned_deal_options: fields.owned_deal_options.map(
       ({
         fields: { original_price, discounted_price, name, total_available },
@@ -39,18 +40,19 @@ export class DealsService {
 
   async getAllDeals(): Promise<ApiDeals | []> {
     try {
-      const response = await this.contentfulService.getContentfulEntries({
-        content_type: 'deal',
-        limit: 1,
-        field: 'title',
-        field_name: 'Burgers and Pints for Two People',
-      });
+      const response =
+        await this.contentfulService.getContentfulEntries<'deal'>({
+          content_type: 'deal',
+          limit: 1,
+          field: 'title',
+          field_name: 'Burgers and Pints for Two People',
+        });
 
       if (!response.items.length) return [];
 
       const cleanedDeals = response.items.map((item) => ({
         id: item.sys.id,
-        // @ts-expect-error error
+        // @ts-expect-error fields.title is a string, but somehow it's expecting a Symbol<string>
         fields: cleanDeals(item.fields),
       }));
 
@@ -67,7 +69,7 @@ export class DealsService {
           dealId,
         );
       if (!response) console.log(response);
-      // @ts-expect-error error
+      // @ts-expect-error fields.title is a string, but somehow it's expecting a Symbol<string>
       return { id: response.sys.id, fields: cleanDeals(response.fields) };
     } catch (err) {
       if (err.id === 'NotFound') {
--- shared/types/deals.ts
+++ shared/types/deals.ts
@@ -3,12 +3,18 @@ import { TypeDealSkeleton } from "@/server/types/generated";
 export interface ApiDeal {
   id: string;
   fields: Omit<
-    TypeDealSkeleton,
-    "title" | "owned_deal_options" | "owner" | "tags" | "banner_images"
+    TypeDealSkeleton["fields"],
+    | "title"
+    | "owned_deal_options"
+    | "owner"
+    | "tags"
+    | "banner_images"
+    | "currency"
   > & {
     title: TypeDealSkeleton["fields"]["title"]["values"];
     tags: TypeDealSkeleton["fields"]["tags"]["item"]["values"][];
     banner_images: string[];
+    currency: TypeDealSkeleton["fields"]["currency"]["values"];
     owner: {
       name: string;
       logo: string;
